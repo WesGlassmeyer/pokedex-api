e4c9fbe5077e52e36f6daaf77e1dd39fd178f2f6
Wes Glassmeyer <wes.glassmeyer@gmail.com>
2020-12-23 16:53:02 -0700

    prep for deply

--- server.js
+++ server.js
@@ -6,11 +6,10 @@ const helmet = require("helmet");
 
 const POKEDEX = require("./pokedex.json");
 
-console.log(process.env.API_TOKEN);
-
 const app = express();
 
-app.use(morgan("dev"));
+const morganSetting = process.env.NODE_ENV === "production" ? "tiny" : "common";
+app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
 
@@ -42,9 +41,6 @@ app.use(function validateBearerToken(req, res, next) {
   if (!authToken || authToken.split(" ")[1] !== apiToken) {
     return res.status(401).json({ error: "Unauthorized request" });
   }
-
-  console.log("validate bearer token middleware");
-  // move to the next middleware
   next();
 });
 
@@ -56,15 +52,12 @@ app.get("/types", handleGetTypes);
 
 function handleGetPokemon(req, res) {
   let response = POKEDEX.pokemon;
-  // filter our pokemon by name if name query param is present
   if (req.query.name) {
     response = response.filter((pokemon) =>
-      // case insensitive searching
       pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
     );
   }
 
-  // filter our pokemon by type if type query param is present
   if (req.query.type) {
     response = response.filter((pokemon) =>
       pokemon.type.includes(req.query.type)
@@ -76,8 +69,16 @@ function handleGetPokemon(req, res) {
 
 app.get("/pokemon", handleGetPokemon);
 
-const PORT = 8000;
-
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
 });
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT);
